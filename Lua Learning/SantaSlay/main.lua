b6d55a2245761b7263e4781358ffd295d86fb52c
EmmaD9 <maduprey1@gmail.com>
2024-12-21 14:53:06 -0500

    added images, and updated my two test main files
    added in template for love 2d api

---
+++ Lua Learning/SantaSlay/main.lua
@@ -0,0 +1,13 @@
+--main lua file for santa slay game
+
+function love.load()
+    
+end
+
+function love.update(dt)
+    
+end
+
+function love.draw()
+    
+end
--- Lua Learning/TestOne/main.lua
+++ Lua Learning/TestOne/main.lua
@@ -1,14 +1,15 @@
 --tutorial from challacade for starting game structure
 
-
 function love.load()
-    number = 0
+    --number = 0
 end
 
 function love.update(dt)
-    number = number + 1
+    --number = number + 1
 end
 
 function love.draw()
-    love.graphics.print(number)
+    --love.graphics.print(number)
+
+    love.graphics.rectangle("fill", 20, 500, 200, 100)
 end
