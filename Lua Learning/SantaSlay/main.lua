b24ffc9802a6a282a972211bccd85e7fde0b4688
EmmaD9 <maduprey1@gmail.com>
2024-12-21 14:59:29 -0500

    Update main.lua

--- Lua Learning/SantaSlay/main.lua
+++ Lua Learning/SantaSlay/main.lua
@@ -1,13 +1,15 @@
---main lua file for santa slay game
+-- Main lua file for Santa Slay game
+
+local image
 
 function love.load()
-    
+    image = love.graphics.newImage("assets/images/santa.png")
 end
 
 function love.update(dt)
-    
+    -- Add your update logic here
 end
 
 function love.draw()
-    
+    love.graphics.draw(image, 0, 0, 0.15, 0.15)
 end
