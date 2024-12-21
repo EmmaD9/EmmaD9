5cba5aab15a0f005b73e4e47865448c5e16afb4c
EmmaD9 <maduprey1@gmail.com>
2024-12-21 15:10:58 -0500

    Update main.lua

--- Lua Learning/TestOne/main.lua
+++ Lua Learning/TestOne/main.lua
@@ -11,6 +11,8 @@ function love.load()
     score = 0
     timer = 0
 
+    gameFont = love.graphics.newFont(40)
+
 end
 
 function love.update(dt)
@@ -21,5 +23,14 @@ function love.draw()
     love.graphics.setColor(1, 0, 0)
     love.graphics.circle("fill", target.x, target.y, target.radius)
 
+    love.graphics.setColor(1,1,1)
+    love.graphics.setFont(gameFont)
     love.graphics.print(score, 0, 0)
+end
+
+function love.mousepressed(x, y, button, istouch, presses)
+    if button == 1 then
+        score = score + 1
+    end
+
 end
