c6e4135d79d52e84c3216f2bfb366421aaac3eda
EmmaD9 <maduprey1@gmail.com>
2024-12-21 14:43:34 -0500

    Update main.lua

--- Lua Learning/TestOne/main.lua
+++ Lua Learning/TestOne/main.lua
@@ -1,3 +1,14 @@
+--tutorial from challacade for starting game structure
+
+
+function love.load()
+    number = 0
+end
+
+function love.update(dt)
+    number = number + 1
+end
+
 function love.draw()
-    love.graphics.print("Hello World", 400, 300)
+    love.graphics.print(number)
 end
