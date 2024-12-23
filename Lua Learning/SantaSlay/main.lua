4767fe7df0d2e90cd50f2b435b2155bcbb00377a
EmmaD9 <maduprey1@gmail.com>
2024-12-23 02:47:19 -0500

    lua stuff

--- Lua Learning/SantaSlay/main.lua
+++ Lua Learning/SantaSlay/main.lua
@@ -1,15 +1,55 @@
 -- Main lua file for Santa Slay game
 
-local image
+local santa
+local canyCane
+
 
 function love.load()
-    image = love.graphics.newImage("assets/images/santa.png")
+    santa = love.graphics.newImage("assets/images/santa.png")
+
+    candyCane = love.graphics.newImage("assets/images/candyCane.png")
+
+
+    --global variable target is a circle
+    target = {}
+    target.x = 650
+    target.y = 100
+    target.radius = 50
+
+    score = 0
+    timer = 0
+
+    gameFont = love.graphics.newFont(40)
+
 end
 
 function love.update(dt)
-    -- Add your update logic here
+    --number = number + 1
 end
 
 function love.draw()
-    love.graphics.draw(image, 0, 0, 0.15, 0.15)
+	
+	love.graphics.draw(santa, 0, 0, 0.15, 0.15)
+	love.graphics.draw(candyCane, 0, 0, 0.15, 0.15)
+
+
+    love.graphics.setColor(1, 0, 0)
+    love.graphics.circle("fill", target.x, target.y, target.radius)
+
+    love.graphics.setColor(1,1,1)
+    love.graphics.setFont(gameFont)
+    love.graphics.print(score, 0, 0)
+end
+
+function love.mousepressed(x, y, button, istouch, presses)
+    if button == 1 and distanceBetween(x, y, target.x, target.y) <= target.radius then
+        score = score + 1
+    end
+
+    --or could do a nested if statement and create a local variable called mouseToTarget
+
+end
+
+function distanceBetween(x1, y1, x2, y2)
+    return math.sqrt((x2 - x1) ^ 2 + (y2 - y1) ^ 2)
 end
--- Lua Learning/TestOne/main.lua
+++ Lua Learning/TestOne/main.lua
@@ -4,8 +4,8 @@ function love.load()
 
     --global variable target is a circle
     target = {}
-    target.x = 300
-    target.y = 300
+    target.x = 650
+    target.y = 100
     target.radius = 50
 
     score = 0
@@ -29,8 +29,14 @@ function love.draw()
 end
 
 function love.mousepressed(x, y, button, istouch, presses)
-    if button == 1 then
+    if button == 1 and distanceBetween(x, y, target.x, target.y) <= target.radius then
         score = score + 1
     end
 
+    --or could do a nested if statement and create a local variable called mouseToTarget
+
+end
+
+function distanceBetween(x1, y1, x2, y2)
+    return math.sqrt((x2 - x1) ^ 2 + (y2 - y1) ^ 2)
 end
